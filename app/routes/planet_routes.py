1a85e07223b6865bf8078a1497b324000fb978c0
Riley Drellishak <rileydrellishak@gmail.com>
2025-10-20 14:18:40 -0400

    Completes Wave 2
    Co-authored-by: Tsz Tin Vivian Soo <tsztin0217@users.noreply.github.com>

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,5 +1,4 @@
-# 1. ...to get all existing `planets`, so that I can see a list of `planets`, with their `id`, `name`, `description`, and other data of the `planet`.
-from flask import Blueprint
+from flask import abort, Blueprint, make_response
 from app.models.planet import planets
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
@@ -10,3 +9,22 @@ def get_all_planets():
     for planet in planets:
         result.append(dict(id=planet.id, name=planet.name, description=planet.description, radius=planet.radius))
     return result
+
+@planets_bp.get('/<planet_id>')
+def get_single_planet(planet_id):
+    planet = validate_planet(planet_id)
+    return dict(id=planet.id, name=planet.name, description=planet.description, radius=planet.radius)
+        
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        response = {'message': f'Planet id ({planet_id}) invalid'}
+        abort(make_response(response, 400))
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+        
+    response = {'message': f'Planet id ({planet_id}) not found'}
+    abort(make_response(response, 404))
