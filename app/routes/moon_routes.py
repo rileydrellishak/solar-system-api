b1d503c6c62dfa738f01c7055bbc56bb926446c2
Riley Drellishak <rileydrellishak@gmail.com>
2025-11-10 09:59:06 -0500

    create moon route

--- app/routes/moon_routes.py
+++ app/routes/moon_routes.py
@@ -13,3 +13,14 @@ def get_all_moons():
     for moon in moons:
         result.append(moon.to_dict())
     return result
+
+@bp.post('')
+def create_moon():
+    request_body = request.get_json()
+
+    new_moon = Moon.from_dict(request_body)
+
+    db.session.add(new_moon)
+    db.session.commit()
+
+    return new_moon.to_dict(), 201
